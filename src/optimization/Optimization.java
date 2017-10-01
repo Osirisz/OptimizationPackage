de5b131981b0e8260f6fe57f6034d6f38ccbdba6
osirisz <earthstroming@hotmail.com>
2017-10-01 13:44:58 +0700

    complete match 2 pair

--- src/optimization/Optimization.java
+++ src/optimization/Optimization.java
@@ -8,45 +8,81 @@ public class Optimization {
         int cpu = sc.nextInt();
         System.out.println("Insert memory(GB): ");
         int memory = sc.nextInt();
-        ArrayList<Integer> packageList = new ArrayList<Integer>();
+        ArrayList<Integer> arrayPreCheckResource = new ArrayList<Integer>(); //เก็บintก่อนไปcheck
+        ArrayList<String> arrayPostCheckResource = new ArrayList<String>(); //เก็บstringคู่ที่checkแล้ว
         
         double packageMemory[] = new double[]{0.5,1,2,4,8,16,32,48,64,16,32,64,128,224};
         double packageCpu[] = new double[]{1,1,2,2,4,8,12,16,20,2,4,8,16,32,};
         double packageCost[] = new double[]{5,10,20,40,80,160,320,480,640,120,240,480,960,1680};
+        double totalCostPackage;
+        String newCheapIndex;
         int cheapIndex = -1;
         //1vm
+
         for (int i = 0; i < packageCpu.length; i++) {
-            for (int j = 0; j < packageMemory.length; j++) {
-                if((packageMemory[j]-0.3)>=(memory) && packageCpu[j]>=cpu){
-                    if(cheapIndex == -1)
-                        cheapIndex = j;
-                    else if(packageCost[j] < packageCost[cheapIndex]){
-                        cheapIndex = j;
+            if((packageMemory[i]-0.3)>=(memory) && packageCpu[i]>=cpu){
+                    if(cheapIndex == -1){
+                        cheapIndex = i+1;
+                        System.out.println("test");
                     }
                     break;
                 }
-            }  
         }
-        //2vm++
-        ArrayList<Integer> arrayMatchPackage = new ArrayList<Integer>();
-        for (int i = 2; i <= cheapIndex; i++) {//จำนวนคู่
-            for (int j = 1; j < cheapIndex; j++) { //add1ถึงน้อยกว่าchaepIndex(ก่อนตำแหน่งสุดท้าย)จะได้ลดจำนวนลงได้อีก
-                arrayMatchPackage.add(j);
-                while(true) { //จับคู่1ถึงน้อยกว่าcheapIndex(ก่อนตำแหน่งสุดท้าย)จะได้ลดจำนวนลงได้อีก
-                    //จับคู่ถึงcheapแล้วเทียบresourceแล้วaddเข้าarray
-                    //จับคุ่ยังไงให้มันครบ
-                }
-                //เอาarrayมาจับคู่ถึงn-1และpopตัวแรกออกไปเรื่อยๆ, ให้array={}
+        //2vm1
+        while(arrayPreCheckResource.isEmpty()){ //checkว่าถ้าว่างให้เข้ามาทำ
+            for (int i = 1; i < cheapIndex; i++) { //loop add 1-cheapIndex-1(ตัวก่อนสุดท้าย)เข้ามาเพื่อcheck
+                arrayPreCheckResource.add(i); //addเลข1-cheapIndex-1(ตัวก่อนสุดท้าย)
             }
+            while(!arrayPreCheckResource.isEmpty()){ //เมื่อมีข้อมูลจึงเข้ามาทำ
+                int firstValue = arrayPreCheckResource.get(0);
+                int secondValue = arrayPreCheckResource.get(0);
+                System.out.println(arrayPreCheckResource.size()-1); //ขนาดarray-1
+                for (int j = 1; j <= arrayPreCheckResource.size(); j++) { //1-ขนาดarray
+                    System.out.println("j"+j);
+                    secondValue = arrayPreCheckResource.get(j-1);//ขยับไปอีกช่อง ช่อง0-3
+                    System.out.println("sec"+secondValue); //value1-ขนาดarray
+                    System.out.println("pack"+firstValue+""+secondValue); //packageที่มี
+                    System.out.println("CPU : " + packageCpu[firstValue-1]+" "+packageCpu[secondValue-1]);
+                    System.out.println("Memory : " + packageMemory[firstValue-1]+" "+packageMemory[secondValue-1]);
+                    System.out.println("size"+arrayPreCheckResource.size()); //size
+                    System.out.println(j+"=="+(arrayPreCheckResource.size())); //ไล่ค่าแต่ละครั้ง               
+               
+                    if(arrayPreCheckResource.size()==1){ //ถ้าในarrayเหลือตัวสุดท้ายให้หยุด
+                        System.out.println("stop");
+                        arrayPreCheckResource.clear(); //clearทุกค่าออกให้หลุดloop
+                    }
+                    if(j==arrayPreCheckResource.size()) { //ถ้าถึงรอบที่วนจับคู่ตัวเดียวกับก็removeตัวแรกออก
+                        System.out.println("remove");
+                        if (!arrayPreCheckResource.isEmpty()) {
+                            arrayPreCheckResource.remove(0);
+                            System.out.println("sizeAR"+arrayPreCheckResource.size());
+                            System.out.println(arrayPreCheckResource);
+                            System.out.println();
+                        }                       
+                    }   
+                    if(((packageMemory[firstValue-1]+packageMemory[secondValue-1])-0.6)>=(memory) && (packageCpu[firstValue-1]+packageCpu[secondValue-1])>=cpu){ //เลือกคู่ที่จะไปadd
+                        totalCostPackage = packageCost[firstValue-1]+packageCost[secondValue-1];
+                        if(cheapIndex == -1){
+                            cheapIndex = 0;
+                        }
+                        else if(totalCostPackage < packageCost[cheapIndex]){
+                            System.err.println("Add");
+                            newCheapIndex = firstValue+""+secondValue;
+                            arrayPostCheckResource.add(newCheapIndex);
+                        }
+                        break;
+                    }
+                }                
+            }
+            break;
         }
-        
-        System.out.println("array"+arrayMatchPackage);
         
         
         System.out.println("Cheap : " + cheapIndex);
         System.out.println("CPU : " + packageCpu[cheapIndex]);
         System.out.println("Memory : " + packageMemory[cheapIndex]);
-        System.out.println("Cost : " + packageCost[cheapIndex]);       
+        System.out.println("Cost : " + packageCost[cheapIndex]); 
+        System.out.println("List Package : " + arrayPostCheckResource);
     }
     
 }
--- src/optimization/test.java
+++ src/optimization/test.java
@@ -16,9 +16,16 @@ public class test {
         int cheapIndex = 5;
         ArrayList<Integer> arrayPreCheckResource = new ArrayList<Integer>(); //เก็บintก่อนไปcheck
         ArrayList<String> arrayPostCheckResource = new ArrayList<String>(); //เก็บstringคู่ที่checkแล้ว
+        double packageMemory[] = new double[]{0.5,1,2,4,8,16,32,48,64,16,32,64,128,224};
+        double packageCpu[] = new double[]{1,1,2,2,4,8,12,16,20,2,4,8,16,32,};
+        double packageCost[] = new double[]{5,10,20,40,80,160,320,480,640,120,240,480,960,1680};
+        double cpu = 2;
+        double memory = 4;
+        double totalCostPackage;
+        String newCheapIndex;
         while(arrayPreCheckResource.isEmpty()){ //checkว่าถ้าว่างให้เข้ามาทำ
-            for (int i = 1; i <= cheapIndex-1; i++) { //loop add 1-cheapIndexเข้ามาเพื่อcheck
-                arrayPreCheckResource.add(i); //addเลข1-cheapIndex
+            for (int i = 1; i < cheapIndex; i++) { //loop add 1-cheapIndex-1(ตัวก่อนสุดท้าย)เข้ามาเพื่อcheck
+                arrayPreCheckResource.add(i); //addเลข1-cheapIndex-1(ตัวก่อนสุดท้าย)
             }
             while(!arrayPreCheckResource.isEmpty()){ //เมื่อมีข้อมูลจึงเข้ามาทำ
                 int firstValue = arrayPreCheckResource.get(0);
@@ -30,9 +37,13 @@ public class test {
                     secondValue = arrayPreCheckResource.get(j-1);//ขยับไปอีกช่อง ช่อง0-3
                     System.out.println("sec"+secondValue); //value1-4
                     System.out.println("pack"+firstValue+""+secondValue); //packageที่มี
+                    System.out.println("CPU : " + packageCpu[firstValue-1]+" "+packageCpu[secondValue-1]);
+                    System.out.println("Memory : " + packageMemory[firstValue-1]+" "+packageMemory[secondValue-1]);
                     System.out.println("size"+arrayPreCheckResource.size()); //size
                     System.out.println(j+"=="+(arrayPreCheckResource.size())); //ไล่ค่าแต่ละครั้ง
                     
+                    
+               
                     if(arrayPreCheckResource.size()==1){ //ถ้าในarrayเหลือตัวสุดท้ายให้หยุด
                         System.out.println("stop");
                         arrayPreCheckResource.clear(); //clearทุกค่าออกให้หลุดloop
@@ -45,11 +56,23 @@ public class test {
                             System.out.println(arrayPreCheckResource);
                             System.out.println();
                         }                       
-                    }                    
+                    }   
+                    if(((packageMemory[firstValue-1]+packageMemory[secondValue-1])-0.6)>=(memory) && (packageCpu[firstValue-1]+packageCpu[secondValue-1])>=cpu){ //เลือกคู่ที่จะไปadd
+                        totalCostPackage = packageCost[firstValue-1]+packageCost[secondValue-1];
+                        if(cheapIndex == -1){
+                            cheapIndex = 0;
+                        }
+                        else if(totalCostPackage < packageCost[cheapIndex]){
+                            newCheapIndex = firstValue+""+secondValue;
+                            arrayPostCheckResource.add(newCheapIndex);
+                        }
+                        break;
+                    }
                 }                
             }
             break;
         }
         System.out.println("end");
+        System.out.println(arrayPostCheckResource);
     }
 }
